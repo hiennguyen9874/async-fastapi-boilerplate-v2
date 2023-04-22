7c44a2fb02844c5ec21d83981926e524923c9ef5
Xuan Hien <hiennguyen9874@gmail.com>
2023-04-22 10:33:21 +0700

    Update pylint

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -1,20 +1,20 @@
+import glob
 import re
 import sys
-import glob
 
 if __name__ == "__main__":
-    version = sys.argv[1]
+    VERSION = sys.argv[1]
 
     for file in glob.glob("./.env*"):
-        with open(file, "r") as f:
+        with open(file, encoding="UTF-8") as f:
             content = f.read()
 
         content_new = re.sub(
-            "APP_VERSION=[a-zA-Z0-9.\-:]+",
-            f"APP_VERSION={version}",
+            r"APP_VERSION=[a-zA-Z0-9.\-:]+",
+            f"APP_VERSION={VERSION}",
             content,
             flags=re.M,
         )
 
-        with open(file, "w") as f:
+        with open(file, "w", encoding="UTF-8") as f:
             f.write(content_new)
